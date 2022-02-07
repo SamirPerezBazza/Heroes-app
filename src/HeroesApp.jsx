87490d34e67b85df5dab819b91fb27c26d36f8b9
Samir Perez <MY_NAME@example.com>
2022-02-07 16:54:24 -0500

    feat: added AuthContext, authReducer and useReducer hook. Implemented Provider to the whole App

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,10 +1,29 @@
+
+import { useReducer } from 'react';
+import { AuthContext } from "./auth/authContext";
+import { authReducer } from './auth/authReducer';
 import { AppRouter } from "./routers/AppRouter";
 
+const init = () => {
+  return {
+    logged: true,
+    name: "Samir (Temporal)"
+  }
+}
+
 const HeroesApp = () => {
+
+  const [user, dispatch] = useReducer(authReducer, {}, init)
+
+  //As a Provider, the Context is giving information to all the
+  //children components
   return (
-    <div>
+    <AuthContext.Provider value={{
+      user,
+      dispatch
+    }}>
       <AppRouter />
-    </div>
+    </AuthContext.Provider>
   );
 };
 
