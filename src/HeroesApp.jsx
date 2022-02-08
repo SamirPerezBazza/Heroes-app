bcd11ad45149ff50f7830f5122b4b1564458813a
Samir Perez <MY_NAME@example.com>
2022-02-08 16:10:26 -0500

    chore: the init state of the reducer is now retrieved from localStorage

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,19 +1,25 @@
 
-import { useReducer } from 'react';
+import { useReducer, useEffect } from 'react';
 import { AuthContext } from "./auth/authContext";
 import { authReducer } from './auth/authReducer';
 import { AppRouter } from "./routers/AppRouter";
 
 const init = () => {
-  return {
-    logged: true,
-    name: "Samir (Temporal)"
-  }
+  return JSON.parse(localStorage.getItem('user')) || { logged: false };
 }
 
 const HeroesApp = () => {
 
+  //The reducer is defined in the top of the component's tree, this will allow 
+  //the access to all the children by the useContext hook.
   const [user, dispatch] = useReducer(authReducer, {}, init)
+
+  useEffect(() => {
+    if (!user) return
+
+    localStorage.setItem('user', JSON.stringify(user));
+  }, [user]);
+
 
   //As a Provider, the Context is giving information to all the
   //children components
