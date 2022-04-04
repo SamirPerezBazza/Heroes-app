8d1b3e107b4d2d00f49e3e238f94d6b1382d4e68
Samir Perez <MY_NAME@example.com>
2022-04-04 15:03:22 -0500

    feat: now the login is a public route and the herolist is a private route

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import LoginScreen from "../components/Login/LoginScreen";
 import DashboardRoutes from "./DashboardRoutes";
+import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
 
 export const AppRouter = () => {
 
@@ -9,8 +11,20 @@ export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginScreen />} />
-        <Route path="/*" element={<DashboardRoutes />} />
+
+        <Route path="/login" element={
+          <PublicRoute>
+            <LoginScreen />
+          </PublicRoute>
+        } />
+
+        <Route path="/*" element={
+          <PrivateRoute>
+            <DashboardRoutes />
+          </PrivateRoute>
+        }
+        />
+
       </Routes>
     </BrowserRouter>
   );
